9dd2aba853e972f752172f17d960f00bb8cdcfc5
Dem01isher <madarthas445@gmail.com>
2021-02-12 01:20:17 +0200

    Update profile

--- app/src/main/java/com/example/kopashop/bottomNav/SettingsFragment.kt
+++ app/src/main/java/com/example/kopashop/bottomNav/SettingsFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.kopashop.databinding.FragmentMenuBinding
+import com.bumptech.glide.Glide
 import com.example.kopashop.databinding.FragmentSettingsBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SettingsFragment : Fragment() {
     private lateinit var binding: FragmentSettingsBinding
+    private lateinit var auth: FirebaseAuth
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +30,16 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser
+
+        binding.nameTxt.text = currentUser?.displayName
+        binding.emailTxt.text = currentUser?.email
+        binding.idText.text = currentUser?.uid
+        binding.numberTxt.text = currentUser?.phoneNumber
+        Glide.with(this).load(currentUser?.photoUrl).into(binding.imageUser)
+
     }
 
 
--- app/src/main/java/com/example/kopashop/fragments/VerifNumFragment.kt
+++ app/src/main/java/com/example/kopashop/fragments/VerifNumFragment.kt
@@ -1,25 +1,18 @@
 package com.example.kopashop.fragments
 
-import android.content.Intent
-import android.icu.util.TimeUnit
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.kopashop.R
 import com.example.kopashop.databinding.FragmentVerifNumBinding
-import com.google.android.material.snackbar.Snackbar
-import com.google.common.base.Strings.repeat
-import com.google.firebase.auth.PhoneAuthProvider
-import com.google.firebase.database.core.Constants
+
 
 class VerifNumFragment : Fragment() {
 
     private lateinit var binding: FragmentVerifNumBinding
-    // This property is only valid between onCreateView and
 // onDestroyView.
 
     override fun onCreateView(
@@ -34,10 +27,14 @@ class VerifNumFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         binding.vrfBtn.setOnClickListener {
             findNavController().navigate(R.id.action_verifNumFragment_to_registerFragment)
         }
+
     }
+
+
 
 
 
